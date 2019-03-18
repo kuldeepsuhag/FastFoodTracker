1c8f91e70690280ab6d4fbe625aa5453019bdb55
RohitAjith <rohitktc@gmail.com>
2019-03-19 09:38:06 +1100

    UI changes

--- FastFoodVisitCounterApp/App.js
+++ FastFoodVisitCounterApp/App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
    let { status } = await Permissions.askAsync(Permissions.LOCATION);
    if (status !== 'granted') {
      this.setState({
-       locationResult: 'Permission to access location was denied',
+       locationResult: 'Location Permissions Needed to proceed',
      });
    } else {
      this.setState({ hasLocationPermissions: true });
@@ -31,7 +31,11 @@ export default class App extends React.Component {
    let location = await Location.getCurrentPositionAsync({});
    this.setState({ locationResult: JSON.stringify(location) });
    
-   // Center the map on the location we just fetched.
+    // The map is sized according to the width and height specified in the styles and/or calculated by react-native.
+    // The map computes two values, longitudeDelta/width and latitudeDelta/height, compares those 2 computed values, and takes the larger of the two.
+    // The map is zoomed according to the value chosen in step 2 and the other value is ignored.
+    // If the chosen value is longitudeDelta, then the left edge is longitude - longitudeDelta and the right edge is longitude + longitudeDelta. The top and bottom are whatever values are needed to fill the height without stretching the map.
+    // If the chosen value is latitudeDelta, then the bottom edge is latitude - latitudeDelta and the top edge is latitude + latitudeDelta. The left and right are whatever values are needed to fill the width without stretching the map.
     this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
   };
 
@@ -39,18 +43,18 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.paragraph}>
-          Pan, zoom, and tap on the map!
+          Fast Food Visit Counter App
         </Text>
         
         {
           this.state.locationResult === null ?
           <Text>Finding your current location...</Text> :
           this.state.hasLocationPermissions === false ?
-            <Text>Location permissions are not granted.</Text> :
+            <Text>Please provide location permissions.</Text> :
             this.state.mapRegion === null ?
             <Text>Map region doesn't exist.</Text> :
             <MapView
-              style={{ alignSelf: 'stretch', height: 400 }}
+              style={{ alignSelf: 'stretch', height: 600 }}
               region={this.state.mapRegion}
               showsUserLocation={true}
               onRegionChange={this._handleMapRegionChange}
