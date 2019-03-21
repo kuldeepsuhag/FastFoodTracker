c69221157305a6609b49b1717f00a5022a42a41a
Akshay Nakra <akshay.nakra@lexxtechnologies.com>
2019-03-21 17:23:26 +1100

    Creating components to segregate code

--- FastFoodVisitCounterApp/App.js
+++ FastFoodVisitCounterApp/App.js
@@ -1,87 +1,14 @@
 import React from 'react';
-import { Constants, MapView, Location, Permissions } from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import Map from './components/Map';
+import { View } from 'react-native';
 
 export default class App extends React.Component {
-  state = {
-    mapRegion: null,
-    hasLocationPermissions: false,
-    locationResult: null
-  };
-
-  componentDidMount() {
-    this._getLocationAsync();
-  }
-
-  _handleMapRegionChange = mapRegion => {
-    console.log(mapRegion);
-    this.setState({ mapRegion });
-  };
-
-  _getLocationAsync = async () => {
-   let { status } = await Permissions.askAsync(Permissions.LOCATION);
-   if (status !== 'granted') {
-     this.setState({
-       locationResult: 'Location Permissions Needed to proceed',
-     });
-   } else {
-     this.setState({ hasLocationPermissions: true });
-   }
-
-   let location = await Location.getCurrentPositionAsync({});
-   this.setState({ locationResult: JSON.stringify(location) });
-   
-    // The map is sized according to the width and height specified in the styles and/or calculated by react-native.
-    // The map computes two values, longitudeDelta/width and latitudeDelta/height, compares those 2 computed values, and takes the larger of the two.
-    // The map is zoomed according to the value chosen in step 2 and the other value is ignored.
-    // If the chosen value is longitudeDelta, then the left edge is longitude - longitudeDelta and the right edge is longitude + longitudeDelta. The top and bottom are whatever values are needed to fill the height without stretching the map.
-    // If the chosen value is latitudeDelta, then the bottom edge is latitude - latitudeDelta and the top edge is latitude + latitudeDelta. The left and right are whatever values are needed to fill the width without stretching the map.
-    this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
-  };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Fast Food Visit Counter App
-        </Text>
-        
-        {
-          this.state.locationResult === null ?
-          <Text>Finding your current location...</Text> :
-          this.state.hasLocationPermissions === false ?
-            <Text>Please provide location permissions.</Text> :
-            this.state.mapRegion === null ?
-            <Text>Map region doesn't exist.</Text> :
-            <MapView
-              style={{ alignSelf: 'stretch', height: 600 }}
-              region={this.state.mapRegion}
-              showsUserLocation={true}
-              onRegionChange={this._handleMapRegionChange}
-            />
-        }
-        
-        <Text>
-          Location: {this.state.locationResult}
-        </Text>
+      <View>
+        <Map />
       </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-  },
-});
+        );
+      }
+    }
