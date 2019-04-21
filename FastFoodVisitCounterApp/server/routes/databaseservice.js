e8d94c130f3179af7f9d939cd1950b6c7f93e717
kuldeepsuhag <kuldeepsuhag1@gmail.com>
2019-04-21 23:07:59 +1000

    Location Detection

--- FastFoodVisitCounterApp/server/routes/databaseservice.js
+++ FastFoodVisitCounterApp/server/routes/databaseservice.js
@@ -1,8 +1,24 @@
+require('firebase/database')
+firebase1 = require('firebase/app');
 module.exports = (req,res) => {
-    const timeOut = 500;
-    setTimeout((function() {
         if (req.body) {
+           console.log("Getting Data")
            console.log(req.body);
+           console.log(req.body.place);
+           var lattt =  -37.8006047;
+           var longii = 144.9636479;
+
+           var ref = firebase1.database().ref("KFC");
+           nextref = ref.child("Carlton").on("value",function (childSnapshot) {
+            data = childSnapshot.val();
+            console.log("Test Karo" + data.name);
+            if (lattt == data.lat && longii == data.long){
+                console.log("Finally")
+            }
+           });
+           res.status(200).send("Successful");
         }
-    }), timeOut);
+        else {
+            res.status(403).end();
+        }
 };
