3fa6e189fb33cc113229a9e8afc329a3888781e3
Akshay Nakra <akshay.nakra@lexxtechnologies.com>
2019-06-04 20:53:34 +1000

    Fix

--- FastFoodVisitCounterApp/server/routes/updateValue.js
+++ FastFoodVisitCounterApp/server/routes/updateValue.js
@@ -2,45 +2,48 @@ require('firebase/database')
 firebase1 = require('firebase/app');
 require('firebase/auth')
 
-module.exports = (req,res) => {
-    if(req.body){
-        firebase.auth().onAuthStateChanged((user)=>{
-            if(user){
-                updateheight(user.uid , req.body.updateValue, req.body.label, res)
+module.exports = (req, res) => {
+    if (req.body) {
+        var updated;
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                updated = updateValue(user.uid, req.body.updateValue, req.body.label, res)
             }
         });
+        if (updated) { res.status(200).send("updated"); }
     }
 }
-function updateheight(uid, updateValue, label, res){
+function updateValue(uid, updateValue, label, res) {
     console.log(label)
-    switch(label){
+    switch (label) {
         case "height":
-                firebase1.database().ref('users').child(uid).update({
-                    height: updateValue
-                })
-                break;
+            firebase1.database().ref('users').child(uid).update({
+                height: updateValue
+            })
+            break;
         case "weight":
-                firebase1.database().ref('users').child(uid).update({
-                    weight: updateValue
-                })
-                break;
+            firebase1.database().ref('users').child(uid).update({
+                weight: updateValue
+            })
+            break;
         case "currentGoal":
-                firebase1.database().ref('users').child(uid).update({
-                    currentGoal: updateValue
-                })
-                break;
+            firebase1.database().ref('users').child(uid).update({
+                currentGoal: updateValue
+            })
+            break;
         case "patient":
-                firebase1.database().ref('users').child(uid).update({
-                    PatientID: updateValue
-                })
-                break;
+            firebase1.database().ref('users').child(uid).update({
+                PatientID: updateValue
+            })
+            break;
         case "doctor":
-                firebase1.database().ref('users').child(uid).update({
-                    doctorId: updateValue
-                })
-                break;
+            firebase1.database().ref('users').child(uid).update({
+                doctorId: updateValue
+            })
+            break;
     }
-    res.status(200).send("updated");
+    return true;
 
-    
+
+
 }
