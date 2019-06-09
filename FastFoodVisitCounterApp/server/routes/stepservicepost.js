3cbcc11ffeb4c3dc35efe7a782d19a567942febb
RohitAjith <rohitktc@gmail.com>
2019-06-10 01:13:09 +1000

    Adding step data using push

--- FastFoodVisitCounterApp/server/routes/stepservicepost.js
+++ FastFoodVisitCounterApp/server/routes/stepservicepost.js
@@ -5,7 +5,9 @@ module.exports = (req, res) => {
     if (req.body) {
         var ref = firebase.database().ref("StepData");
         userRef = ref.child(req.body.uid)
-        userRef.set(req.body.stepData);
+        for (let i = 0; i < req.body.stepData.length; i++) {
+            userRef.push(req.body.stepData[i]);
+        }
         res.status(200).send("Successful");
     } else {
         res.status(403).end();
